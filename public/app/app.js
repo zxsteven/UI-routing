840fcc4fc8a4dde7704e9ee667c3a4da4a97a0b3
Steven Buxton <breathesteve@gmail.com>
2016-01-05 10:32:20 -0600

    fixed routes
    fixed routes

--- public/app/app.js
+++ public/app/app.js
@@ -4,11 +4,65 @@ var app = angular.module('app', ['ui.router'])
 
 	$logProvider.debugEnabled(true);
 
-	$stateProvider
-		.state('name', {
-			url: '/',
-			templateUrl: '/public/app/templates/home.html',
-			controller: 'HomeController',
-			controllerAs: 'home'
-		});
+		$stateProvider
+			.state('home', {
+					url: '/',
+					templateUrl: '/public/app/templates/home.html',
+					controller: 'HomeController',
+					controllerAs: 'home'
+			})
+			.state('schools', {
+					url: '/schools',
+					controller: 'AllSchoolsController',
+					controllerAs: 'schools',
+					templateUrl: '/public/app/templates/allSchools.html'
+			})
+			.state('classrooms', {
+					url: '/classrooms',
+					controller: 'AllClassroomsController',
+					controllerAs: 'classrooms',
+					templateUrl: '/public/app/templates/allClassrooms.html',
+					onEnter: function ($log) {
+							$log.debug('Entering the classrooms state.');
+					},
+					onExit: function ($log) {
+							$log.debug('Exiting the classrooms state.');
+					}
+			})
+			.state('activities', {
+					url: '/activities',
+					controller: 'AllActivitiesController',
+					controllerAs: 'activities',
+					templateUrl: '/public/app/templates/allActivities.html',
+					resolve: {
+							activities: function (dataService) {
+									return dataService.getAllActivities();
+							}
+					},
+					data: {
+							name: 'My Activity',
+							desc: 'Fun!'
+					},
+					foo: {
+							myFoo: 'bar'
+					}
+			})
+			.state('classroom_summary', {
+					url: '/classrooms/:id',
+					templateUrl: '/public/app/templates/classroom.html',
+					controller: 'ClassroomController',
+					controllerAs: 'classroom'
+			})
+			.state('classroom_detail', {
+					url: '/classrooms/{id:[0-9]}/detail/{month}',
+					templateUrl: '/public/app/templates/classroomDetail.html',
+					controller: 'ClassroomController',
+					controllerAs: 'classroom',
+					params: {
+							classroomMessage: { value: 'Learning is fun!' }
+					}
+			});
+
+
 }]);
+
